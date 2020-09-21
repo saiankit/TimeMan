9359fb51f75ed196c233c8ebda029656698888e0
saiankit <saiankit30@gmail.com>
2020-09-21 10:30:30 +0530

    Initial Commit

---
+++ TimeMan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeMan
+//
+//  Created by Sai Ankit on 9/21/20.
+//  Copyright © 2020 Sai Ankit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
