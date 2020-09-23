80f147e3b84e97d727933df82e39b9c07ec5a58f
saiankit <saiankit30@gmail.com>
2020-09-23 20:59:37 +0530

    Used the Combine Framework to combine the ViewModel and View of CourseInput using Publishers and Subscribers

--- TimeMan/ViewModels/CourseViewModel.swift
+++ TimeMan/ViewModels/CourseViewModel.swift
@@ -13,4 +13,22 @@ class CourseViewModel: ObservableObject {
     @Published var courseTitle : String = ""
     @Published var courseCode : String = ""
     @Published var courseID : String = ""
+    
+    
+    @Published var lectureInstructorName : String = ""
+    @Published var lectureNumber : Int = 1
+    @Published var lectureTime = Date()
+    @Published var lectureMeetLink: String = ""
+    
+    
+    @Published var tutorialInstructorName : String = ""
+    @Published var tutorialNumber : Int = 1
+    @Published var tutorialTime = Date()
+    @Published var tutorialMeetLink: String = ""
+    
+    
+    @Published var practicalInstructorName : String = ""
+    @Published var practicalNumber : Int = 1
+    @Published var practicalTime = Date()
+    @Published var practicalMeetLink: String = ""
 }
--- TimeMan/Views/CourseInput.swift
+++ TimeMan/Views/CourseInput.swift
@@ -9,45 +9,27 @@
 import SwiftUI
 
 struct CourseInput: View {
-    
-    @State var courseTitle : String = ""
-    @State var courseCode : String = ""
-    @State var courseID : String = ""
-    
-    @State var lectureInstructorName : String = ""
-    @State var lectureNumber : Int = 1
-    @State var lectureTime = Date()
-    @State var lectureMeetLink: String = ""
-    
+    @ObservedObject var viewModel: CourseViewModel
     @State var isTutorialExisting : Bool = false
-    @State var tutorialInstructorName : String = ""
-    @State var tutorialNumber : Int = 1
-    @State var tutorialTime = Date()
-    @State var tutorialMeetLink: String = ""
-    
     @State var isPracticalExisting : Bool = false
-    @State var practicalInstructorName : String = ""
-    @State var practicalNumber : Int = 1
-    @State var practicalTime = Date()
-    @State var practicalMeetLink: String = ""
     
     var body: some View {
         Form{
             Section(header: Text("Course Information")){
-                TextField("Course Title ( Digital Design )", text: $courseTitle)
+                TextField("Course Title ( Digital Design )", text: $viewModel.courseTitle)
                 
-                TextField("Course Code ( ECE )", text: $courseCode)
+                TextField("Course Code ( ECE )", text: $viewModel.courseCode)
                 
-                TextField("Course ID ( F215 )", text: $courseID)
+                TextField("Course ID ( F215 )", text: $viewModel.courseID)
             }
             // MARK: - Lecture
             Section(header: Text("Lecture Information")){
-                TextField("Lecture Instructor", text: $lectureInstructorName)
-                Stepper(value: $lectureNumber, in: 1...10){
-                    Text("L" + String(lectureNumber))
+                TextField("Lecture Instructor", text: $viewModel.lectureInstructorName)
+                Stepper(value: $viewModel.lectureNumber, in: 1...10){
+                    Text("L" + String(viewModel.lectureNumber))
                 }
-                DatePicker("Lecture Time", selection: $lectureTime, displayedComponents: .hourAndMinute)
-                TextField("Lecture Meet Link", text: $lectureMeetLink)
+                DatePicker("Lecture Time", selection: $viewModel.lectureTime, displayedComponents: .hourAndMinute)
+                TextField("Lecture Meet Link", text: $viewModel.lectureMeetLink)
             }
             
             
@@ -59,12 +41,12 @@ struct CourseInput: View {
             if(self.isTutorialExisting)
             {
                 Section(header: Text("Tutorial Information")){
-                    TextField("Tutorial Instructor", text: $tutorialInstructorName)
-                    Stepper(value: $tutorialNumber, in: 1...10){
-                                      Text("T" + String(tutorialNumber))
+                    TextField("Tutorial Instructor", text: $viewModel.tutorialInstructorName)
+                    Stepper(value: $viewModel.tutorialNumber, in: 1...10){
+                        Text("T" + String(viewModel.tutorialNumber))
                                   }
-                    DatePicker("Tutorial Time", selection: $tutorialTime, displayedComponents: .hourAndMinute)
-                    TextField("Tutorial Meet Link", text: $tutorialMeetLink)
+                    DatePicker("Tutorial Time", selection: $viewModel.tutorialTime, displayedComponents: .hourAndMinute)
+                    TextField("Tutorial Meet Link", text: $viewModel.tutorialMeetLink)
                     
                 }
             }
@@ -78,12 +60,12 @@ struct CourseInput: View {
             if(self.isPracticalExisting)
             {
                 Section(header: Text("Practical Information")){
-                    TextField("Practical Instructor", text: $practicalInstructorName)
-                    Stepper(value: $practicalNumber, in: 1...10){
-                                      Text("P" + String(practicalNumber))
+                    TextField("Practical Instructor", text: $viewModel.practicalInstructorName)
+                    Stepper(value: $viewModel.practicalNumber, in: 1...10){
+                        Text("P" + String(viewModel.practicalNumber))
                                   }
-                    DatePicker("Practical Time", selection: $practicalTime, displayedComponents: .hourAndMinute)
-                    TextField("Practical Meet Link", text: $practicalMeetLink)
+                    DatePicker("Practical Time", selection: $viewModel.practicalTime, displayedComponents: .hourAndMinute)
+                    TextField("Practical Meet Link", text: $viewModel.practicalMeetLink)
                 }
             }
         }.padding()
@@ -92,6 +74,6 @@ struct CourseInput: View {
 
 struct CourseInput_Previews: PreviewProvider {
     static var previews: some View {
-        CourseInput()
+        Text("Hello")
     }
 }
