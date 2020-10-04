8ee5f7edbc0f23836826c465ae3b84f34ea777b0
saiankit <saiankit30@gmail.com>
2020-10-04 17:17:15 +0530

    Modified the FAB

---
+++ TimeMan/Views/Components/ScrollScreenComponents/FloatingActionButton.swift
@@ -0,0 +1,36 @@
+//
+//  FloatingActionButton.swift
+//  TimeMan
+//
+//  Created by Sai Ankit on 10/4/20.
+//  Copyright © 2020 Sai Ankit. All rights reserved.
+//
+
+import SwiftUI
+struct FloatingActionButton : View {
+    @Binding var isPresented : Bool
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {
+                    self.isPresented.toggle()
+                },
+                label: {
+                    Text("+")
+                        .font(.system(.largeTitle))
+                        .frame(width: 50, height: 45)
+                        .foregroundColor(Color.white)
+                        .padding(.bottom, 7)
+                }
+                )
+                .background(Color("CourseCardSecondaryAccent"))
+                .cornerRadius(25)
+                .padding()
+                .shadow(color: Color.black.opacity(0.3),
+                        radius: 3,x: 3,y: 3)
+            }
+        }
+    }
+}
--- TimeMan/Views/Screens/ScrollScreen.swift
+++ TimeMan/Views/Screens/ScrollScreen.swift
@@ -13,54 +13,32 @@ struct ScrollScreen: View {
     @State var calendarIndex = ((Calendar.current.component(.weekday, from: Date())) - 1)
     var body: some View {
         if #available(iOS 14.0, *) {
-            
             ZStack{
                 ScrollView(showsIndicators: false){
-                          ZStack {
-                            Color("Background")
-                              VStack {
-                                WeekScroll(index: $calendarIndex).padding(.top,20)
-                                  VStack(alignment: .leading){
-                                      HStack {
-                                          Text(calendarIndex == (Calendar.current.component(.weekday, from: Date()) - 1) ? "Today's Classes" : longWeekDaySymbols[calendarIndex] + "'s Classes")
-                                              .font(.system(size: 20, weight: .bold, design: .rounded)).padding(.bottom,15)
-                                          Spacer()
-                                      }
-                                    CoursesList(calendarIndex: $calendarIndex).sheet(isPresented: $isPresented){
-                                        CourseInput(isPresented: $isPresented)
-                                      }
-                                    }
-                                  .frame(minHeight: 800.0,alignment: .top)
-                                  .padding(25)
-                                  .background(Color("CoursesListBackground"))
-                                  .clipShape(CustomCorner(corners: [.topLeft,.topRight], size: 55))
-                    }
-                }
-            }
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            self.isPresented.toggle()
-                        },
-                               label: {
-                                Text("+")
-                                .font(.system(.largeTitle))
-                                .frame(width: 50, height: 45)
-                                .foregroundColor(Color.white)
-                                .padding(.bottom, 7)
+                    ZStack {
+                        Color("Background")
+                        VStack {
+                            WeekScroll(index: $calendarIndex).padding(.top,20)
+                            VStack(alignment: .leading){
+                                HStack {
+                                    Text(calendarIndex == (Calendar.current.component(.weekday, from: Date()) - 1) ? "Today's Classes" : longWeekDaySymbols[calendarIndex] + "'s Classes")
+                                        .font(.system(size: 20, weight: .bold, design: .rounded)).padding(.bottom,15)
+                                    Spacer()
                                 }
-                        )
-                        .background(Color.blue)
-                        .cornerRadius(25)
-                        .padding()
-                        .shadow(color: Color.black.opacity(0.3),
-                        radius: 3,x: 3,y: 3)
+                                CoursesList(calendarIndex: $calendarIndex).sheet(isPresented: $isPresented){
+                                    CourseInput(isPresented: $isPresented)
+                                }
+                            }
+                            .frame(minHeight: 800.0,alignment: .top)
+                            .padding(25)
+                            .background(Color("CoursesListBackground"))
+                            .clipShape(CustomCorner(corners: [.topLeft,.topRight], size: 55))
+                        }
                     }
                 }
+                FloatingActionButton(isPresented: $isPresented)
             }
-     
+            
+        }
     }
-}
 }
