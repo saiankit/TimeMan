4a35b025c82ac79cf36d67a011f55a06bc36665a
saiankit <saiankit30@gmail.com>
2020-10-04 14:18:50 +0530

    Added Grid Components

--- TimeMan/Views/Screens/GridScreen.swift
+++ TimeMan/Views/Screens/GridScreen.swift
@@ -8,33 +8,6 @@
 
 import SwiftUI
 
-
-struct GridWeekItem : View{
-    var title : String
-    var body: some View {
-        VStack{
-            Text(title).font(.system(size: 18, weight: .bold, design: .rounded))
-        }
-        .frame(width: width, height: height)
-        .padding(2)
-    }
-}
-
-struct GridWeekRow : View{
-    var body: some View {
-        HStack(spacing: spacing){
-            GridWeekItem(title: "")
-            GridWeekItem(title: "M")
-            GridWeekItem(title: "T")
-            GridWeekItem(title: "W")
-            GridWeekItem(title: "T")
-            GridWeekItem(title: "F")
-            GridWeekItem(title: "S")
-        }
-    }
-}
-
-
 var height : CGFloat = 40
 var width : CGFloat = 40
 var spacing : CGFloat = 8
@@ -87,53 +60,3 @@ struct GridItem : View {
         }
     }
 }
-
-
-struct Line : View{
-    var body: some View {
-        Rectangle()
-            .fill(Color.gray)
-            .frame(height: 2)
-        
-    }
-}
-
-struct RowForGrid : View{
-    var time : Int
-    
-    var body: some View {
-        VStack {
-            Line()
-            HStack(spacing: spacing){
-                Text(String(time) + ":00").font(.system(size: 10)).frame(width: width, height: height)
-                GridItem(time: time, weekDay: 1)
-                GridItem(time: time, weekDay: 2)
-                GridItem(time: time, weekDay: 3)
-                GridItem(time: time, weekDay: 4)
-                GridItem(time: time, weekDay: 5)
-                GridItem(time: time, weekDay: 6)
-            }
-        }
-    }
-}
-
-
-
-
-struct TimeGrid : View{
-    var body: some View {
-        VStack {
-            RowForGrid(time: 7)
-            RowForGrid(time: 8)
-            RowForGrid(time: 9)
-            RowForGrid(time: 10)
-            RowForGrid(time: 11)
-            RowForGrid(time: 12)
-            RowForGrid(time: 13)
-            RowForGrid(time: 14)
-            RowForGrid(time: 15)
-            RowForGrid(time: 16)
-        }
-    }
-}
-
