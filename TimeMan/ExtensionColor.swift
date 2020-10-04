aec02db2769282adbad8127c934dd9b6bf4ade11
saiankit <saiankit30@gmail.com>
2020-10-04 10:31:45 +0530

    Created a non-working grid view

--- TimeMan/ExtensionColor.swift
+++ TimeMan/ExtensionColor.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 extension UIColor {
     convenience init(hexString: String, alpha: CGFloat = 1.0) {
         let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
--- TimeMan/Views/Screens/GridScreen.swift
+++ TimeMan/Views/Screens/GridScreen.swift
@@ -8,19 +8,76 @@
 
 import SwiftUI
 
+var timeline  = ["7:00","5:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00"]
+
 struct GridScreen: View {
-    @State var calendarIndex = ((Calendar.current.component(.weekday, from: Date())) - 1)
+    
     var body: some View {
         if #available(iOS 14.0, *) {
-            
-            VStack(alignment: .center) {
-                Spacer()
-                    HStack {
-                        Text("Under Construction").font(.system(size: 22, weight: .bold, design: .rounded)).padding(.bottom, 5)
-                    }
-                Spacer()
-                    
-                }
+            ScrollView{
+            HStack(spacing: 50){
+                Text("")
+                Text("M")
+                Text("T")
+                Text("W")
+                Text("T")
+                Text("F")
+                Text("S")
+            }
+                ForEach(0 ..< 13) {_ in
+                RowForGrid()
+            }
         }
     }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridScreen()
+           
+           
+    }
+}
+
+struct GridItem : View {
+    var title : String
+    var body: some View {
+        VStack{
+            Text("MATH").font(.system(size: 10))
+            Text("F215").font(.system(size: 10))
+            Text("L10").font(.system(size: 10))
+        }
+        .padding(2)
+        .background(Color.orange).frame(width: 50, height: 50)
+        
+    }
+}
+
+
+struct Line : View{
+    var body: some View {
+        Rectangle()
+            .fill(Color.black)
+            .frame(height: 2)
+        
+    }
+}
+
+struct RowForGrid : View{
+    var body: some View {
+        VStack {
+            Line()
+            HStack{
+                Text("7:00").font(.system(size: 10))
+                GridItem(title: "ECE F215 L2")
+                GridItem(title: "ECE F215 L2")
+                GridItem(title: "ECE F215 L2")
+                GridItem(title: "ECE F215 L2")
+                GridItem(title: "ECE F215 L2")
+                GridItem(title: "ECE F215 L2")
+            }
+        }.padding(.leading,2)
+        .padding(.trailing,2)
+    }
+}
 }
