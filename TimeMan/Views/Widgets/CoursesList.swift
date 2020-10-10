29dc9ebb522ec25de9bd5343e7a1ac0962d60d37
saiankit <saiankit30@gmail.com>
2020-10-10 17:44:46 +0530

    Updated the CourseList View

--- TimeMan/Views/Widgets/CoursesList.swift
+++ TimeMan/Views/Widgets/CoursesList.swift
@@ -11,28 +11,38 @@ import SwiftUI
 struct CoursesList: View {
     @Binding var calendarIndex: Int
     @FetchRequest(entity: Course.entity(), sortDescriptors: [NSSortDescriptor(key: "time", ascending: true)]) var courseItemsList: FetchedResults<Course>
-    var courseViewModel = CourseListViewModel()
+    var courseListViewModel = CourseListViewModel()
+    
     var body: some View {
         if #available(iOS 14.0, *) {
-            LazyVStack(alignment: .leading) {
-                ForEach(self.courseItemsList, id: \.self) {
-                    if courseViewModel.shouldCourseBeIncluded(course: $0, index: self.calendarIndex) {
-                        CourseCard(course: $0)
+            if !courseListViewModel.areClassesAvailableToday(coursesList: courseItemsList, index: calendarIndex) {
+                NoClassesToday()
+            } else {
+                LazyVStack(alignment: .leading) {
+                    ForEach(self.courseItemsList, id: \.self) {
+                        if courseListViewModel.shouldCourseBeIncluded(course: $0, index: self.calendarIndex) {
+                            CourseCard(course: $0)
+                        }
                     }
                 }
             }
-            if courseViewModel.areClassesAvailableToday(coursesList: courseItemsList, index: calendarIndex) == false {
-                    Text("No Classes Today")
-                        .font(
-                            .system(
-                                size: 22,
-                                weight: .bold,
-                                design: .rounded
-                            )
-                        )
-                        .padding(.bottom, 5)
-                        .padding(.leading,20)
+        }
+    }
+}
+
+struct NoClassesToday: View {
+    var body: some View {
+        VStack{
+            Spacer()
+            HStack{
+                Spacer()
+                Text("No Classes Today")
+                    .font(.system(size: 22, weight: .bold, design: .rounded ))
+                    .padding(.bottom, 5)
+                    .padding(.leading,20)
+                Spacer()
             }
+            Spacer()
         }
     }
 }
